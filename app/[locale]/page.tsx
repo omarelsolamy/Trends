8ef6537a8007b14af3b576c9a5a331269581e234
omarelsolamy <omar.elsolamy@gmail.com>
2026-02-16 10:01:42 +0200

    Update page.tsx

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -3,6 +3,8 @@ import HeroCarousel from '@/components/HeroCarousel';
 import FeaturedArticles from '@/components/FeaturedArticles';
 import ChatButton from '@/components/ChatButton';
 
+export const runtime = 'edge';
+
 export default function Home() {
   return (
     <main className="relative min-h-screen">
