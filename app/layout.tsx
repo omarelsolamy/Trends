f51e316659328efb797c64e4cd96ec36077270f4
omarelsolamy <omar.elsolamy@gmail.com>
2026-02-16 10:55:24 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,23 @@
+import type { Metadata, Viewport } from "next";
+
+export const metadata: Metadata = {
+  title: "Trends",
+  description: "Smart AI Assistant",
+  manifest: "/manifest.json", // 👈 هذا هو الرابط بملف الإعدادات
+  icons: {
+    apple: "/logo.png", // أيقونة الآيفون
+    icon: "/logo.png",  // الأيقونة العامة
+  },
+};
+
+export const viewport: Viewport = {
+  themeColor: "#ffffff", // لون شريط الحالة في الموبايل
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false, // يمنع التكبير ليعطي شعور التطبيق الحقيقي
+};
+
 export default function RootLayout({
   children,
 }: {
